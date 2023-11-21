19a46cbc88d71ccd52358eb181876b186b6e910d
Catalina Segura <catalina.segura@vadesecure.com>
2023-11-21 21:05:26 +0100

    feat: close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,25 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+// DOM Elements (modal)
+const modalBtn = document.querySelector(".modal-btn");
+const modalBg = document.querySelector(".bground");
+const modalContent = document.querySelector(".content");
+const closeModalBtn = document.querySelectorAll(".close");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// Launch modal event
+modalBtn.addEventListener("click", launchModal);
 
-// launch modal form
+// Launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
+  modalContent.style.display = "block";
 }
 
+// Launch close event
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
+// Close modal form
+function closeModal() {
+  modalBg.style.display = "none";
+}

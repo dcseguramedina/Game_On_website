1f5242858c895a6f3a1d3509200f227410bcb592
Catalina Segura <catalina.segura@vadesecure.com>
2023-11-21 21:10:02 +0100

    feat: implement field inputs (firstName, lastName, email) implement field and field validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,3 +29,63 @@ closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 function closeModal() {
   modalBg.style.display = "none";
 }
+
+// Implement form inputs
+
+// Create field class in order to simplify field's validation
+class Field {
+  // Define details
+  constructor(docName, regex, docErrorMsg, errorMsg) {
+    this.docName = docName;
+    this.regex = regex;
+    this.docErrorMsg = docErrorMsg;
+    this.errorMsg = errorMsg;
+  }
+  // Define methods
+  // Add an event to listen to the changes and check the inputs 
+  addEvent() {
+    this.docName.addEventListener("focusout", (event) => {
+      event.preventDefault();
+      this.checkField();
+    })
+  }
+  // Check the field and display an error message if needed
+  checkField() {
+    let valid = this.regex.test(this.docName.value);
+    if (valid === false) {
+      this.docName.style.border = "2px outset red";
+      this.docErrorMsg.textContent = this.errorMsg;
+      return false
+    } else {
+      this.docName.style.border = "none";
+      this.docErrorMsg.textContent = "";
+      return true
+    }
+  }
+}
+
+// Create field object (firstName, lastName, email)
+// Add eventListener (firstName, lastName, email)
+let firstName = new Field(
+  document.getElementById("firstName"),
+  new RegExp(/^(?=.*[A-Za-z].*[a-zA-Z])[A-Za-z\é\è\ê\ë\ï\œ\-\s]+$/),
+  document.getElementById("firstNameErrorMsg"),
+  `Veuillez entrer 2 caractères valides ou plus pour le champ du prénom`
+);
+firstName.addEvent();
+
+let lastName = new Field(
+  document.getElementById("lastName"),
+  new RegExp(/^(?=.*[A-Za-z].*[a-zA-Z])[A-Za-z\é\è\ê\ë\ï\œ\-\s]+$/),
+  document.getElementById("lastNameErrorMsg"),
+  `Veuillez entrer 2 caractères valides ou plus pour le champ du nom`
+);
+lastName.addEvent();
+
+let email = new Field(
+  document.getElementById("email"),
+  new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+[@]{1}[A-Za-z0-9.-]+\.[\w-]{2,4}$/),
+  document.getElementById("emailErrorMsg"),
+  `Veuillez entrer une adresse mail valide`
+);
+email.addEvent();
